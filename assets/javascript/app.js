df4cbbd458ce3b72f3b164693f7bb3d4516f868c
juney <juhnnie@gmail.com>
2019-05-25 01:09:45 -0700

    dsa

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 
 
 var timeleft = 120;
+
 var downloadTimer = setInterval(function(){
   document.getElementById("time").innerHTML ="Time remaining: " + timeleft + " seconds";
   timeleft -= 1;
@@ -9,3 +10,7 @@ var downloadTimer = setInterval(function(){
     document.getElementById("time").innerHTML = "Finished"
   }
 }, 1000);
+
+
+
+
