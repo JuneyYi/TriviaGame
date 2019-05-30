ff68a3f98159b3f8d71cd81866c5a74e85cb5bbf
juney <juhnnie@gmail.com>
2019-05-30 14:34:45 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,15 @@
 
 var timeleft = 120;
 
-var downloadTimer = setInterval(function(){
+var timer = setInterval(function(){
   document.getElementById("time").innerHTML ="Time remaining: " + timeleft + " seconds";
   timeleft -= 1;
   if(timeleft <= 0){
-    clearInterval(downloadTimer);
+    clearInterval(timer);
     document.getElementById("time").innerHTML = "Finished"
   }
 }, 1000);
+
 
 
 
